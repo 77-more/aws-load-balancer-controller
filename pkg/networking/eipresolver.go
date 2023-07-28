5304195539d549d34e5c2f9b350e71fe4e3750a2
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-07-28 13:29:12 -0700

    Update eipresolver.go

--- pkg/networking/eipresolver.go
+++ pkg/networking/eipresolver.go
@@ -24,7 +24,7 @@ func ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
 	}
 	var resolvedEIPs []*ec2sdk.Address
 	if len(allocationIDs) > 0 {
-		eips, err := ec2sdk.DescribeAddressesWithContext(&services.ec2.DescribeAddressesInput{
+		eips, err := ec2sdk.DescribeAddresses(&services.ec2.DescribeAddressesInput{
 			AllocationIds: awssdk.StringSlice(allocationIDs),
 		})
 		if err != nil {
@@ -36,7 +36,7 @@ func ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
 	var availableEIPs []string
 	var unavailableEIPs []string
 	if len(eipsNames) > 0 {
-		describeaddressesoutput, err := ec2sdk.DescribeAddressesWithContext(&services.ec2.DescribeAddressesInput{
+		describeaddressesoutput, err := ec2sdk.DescribeAddresses(&services.ec2.DescribeAddressesInput{
 			Filters: []*ec2.Filter{
 				{
 					Name:   aws.String("tag:Name"),
