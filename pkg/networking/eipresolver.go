0056c9ed4162d05fc5458af87224bde5200ec2a4
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-07-31 11:26:36 -0700

    Update eipresolver.go

--- pkg/networking/eipresolver.go
+++ pkg/networking/eipresolver.go
@@ -9,17 +9,12 @@ import (
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/services"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
-func NewEC2(session *session.Session) EC2 {
-	return &defaultEC2{
-		EC2API: ec2.New(session),
-	}
+type defaultEIPInfoProvider struct {
+	ec2Client        services.EC2
 }
 
-type defaultEC2 struct {
-	ec2iface.EC2API
-}
 
-func ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
+func (p *defaultEIPInfoProvider) ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
 	// Creates session object
 	// makes DescribeAddresses api call and stores the output of type DescribeAddressesOutput in results variable. As part of the API call we are looking for Name: test1 and cluster-name:test tags
 	// &ec2.DescribeAddressesInput represents the memory address of the
@@ -32,9 +27,9 @@ func ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
 			eipsNames = append(eipsNames, nameOrID)
 		}
 	}
-	var resolvedEIPs []*EC2.Address
+	var resolvedEIPs []*ec2sdk.Address
 	if len(allocationIDs) > 0 {
-		eips, err := ec2sdk.DescribeAddresses(&EC2.DescribeAddressesInput{
+		eips, err := p.ec2Client.DescribeAddresses(&ec2sdk.DescribeAddressesInput{
 			AllocationIds: awssdk.StringSlice(allocationIDs),
 		})
 		if err != nil {
@@ -46,8 +41,8 @@ func ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
 	var availableEIPs []string
 	var unavailableEIPs []string
 	if len(eipsNames) > 0 {
-		describeaddressesoutput, err := EC2.DescribeAddresses(&EC2.DescribeAddressesInput{
-			Filters: []*EC2.Filter{
+		describeaddressesoutput, err := p.ec2Client.DescribeAddresses(&ec2sdk.DescribeAddressesInput{
+			Filters: []*ec2sdk.Filter{
 				{
 					Name:   aws.String("tag:Name"),
 					Values: aws.StringSlice(eipsNames),
