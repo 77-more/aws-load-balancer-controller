3c1fabf2fb0ddf4950454fd0993b825fe4244f45
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-07-31 16:02:18 -0700

    Update eipresolver.go

--- pkg/networking/eipresolver.go
+++ pkg/networking/eipresolver.go
@@ -1,70 +1,53 @@
 package networking
 
 import (
-	"fmt"
+	//"fmt"
 	"strings"
 	awssdk "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	ec2sdk "github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/services"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
 type defaultEIPInfoProvider struct {
 	ec2Client        services.EC2
+	]
 }
 
-
-func (p *defaultEIPInfoProvider) ResolveviaNameorAllocationID(eipsNameOrIDs []string) {
+var availableEIPs []string
+var unavailableEIPs []string
+func (p *defaultEIPInfoProvider) eipresolver(EIPnameOrIDs []string) ([]string) {
 	// Creates session object
+	//sess, _ := session.NewSession()
+	// opens a new session
+	//ec2svc := ec2.New(sess)
 	// makes DescribeAddresses api call and stores the output of type DescribeAddressesOutput in results variable. As part of the API call we are looking for Name: test1 and cluster-name:test tags
 	// &ec2.DescribeAddressesInput represents the memory address of the
-	var allocationIDs []string
-	var eipsNames []string
-	for _, nameOrID := range eipsNameOrIDs {
-		if strings.HasPrefix(nameOrID, "eipalloc-") {
-			allocationIDs = append(allocationIDs, nameOrID)
-		} else {
-			eipsNames = append(eipsNames, nameOrID)
-		}
-	}
-	var resolvedEIPs []*ec2sdk.Address
-	if len(allocationIDs) > 0 {
-		eips, _ := p.ec2Client.DescribeAddresses(&ec2sdk.DescribeAddressesInput{
-			AllocationIds: awssdk.StringSlice(allocationIDs),
-		})
-		//if err != nil {
-		//	return nil, err
-		//}
-		resolvedEIPs = append(resolvedEIPs, eips...)
-		return resolvedEIPs
-	}
-	var availableEIPs []string
-	var unavailableEIPs []string
-	if len(eipsNames) > 0 {
-		describeaddressesoutput, _ := p.ec2Client.DescribeAddresses(&ec2sdk.DescribeAddressesInput{
-			Filters: []*ec2sdk.Filter{
+	for _, nameOrIDs := range EIPnameOrIDs {
+		results, _ := p.ec2Client.DescribeAddresses(&ec2.DescribeAddressesInput{
+			Filters: []*ec2.Filter{
 				{
 					Name:   aws.String("tag:Name"),
-					Values: aws.StringSlice(eipsNames),
+					Values: aws.StringSlice([]string{nameOrIDs}),
 				},
 			},
 		})
-		//if err != nil {
-		//	return nil, err
-		//}
+		
+		var allocationIDs []string
+		var associationIDs []string
 
-		for _, address := range describeaddressesoutput.Addresses {
-			allocationIDs = append(allocationIDs, *address.AllocationId)
-			associationIDs = append(associationIDs, *address.AssociationId)
-			if len(associationIDs) > 0 {
-				unavailableEIPs = append(unavailableEIPs, *address.AllocationId)
-			}
-			associationIDs = nil
+		allocationIDs = append(allocationIDs, *results.Addresses[0].AllocationId)
+		associationIDs = append(associationIDs, *results.Addresses[0].AssociationId)
+		if len(associationIDs) > 0 {
+			unavailableEIPs = append(unavailableEIPs, *results.Addresses[0].AllocationId)
+		} else {
+			availableEIPs = append(availableEIPs, *results.Addresses[0].AllocationId)
 		}
-		if len(availableEIPs) == len(eipsNameOrIDs) {
-			resolvedEIPs = append(resolvedEIPs, allocationIDs)
-			return resolvedEIPs
-		}
-
-	}
+		associationIDs = nil
+	
+		} 
+	//	fmt.Printf("available EIPs:%v, unavailableEIPs:%v", availableEIPs, unavailableEIPs)
+        
+        return availableEIPs
+}
 }
