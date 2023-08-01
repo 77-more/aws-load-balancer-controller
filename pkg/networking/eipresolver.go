3b0ca8e70e1e3ab7dbb3d88d4e4aff3de8f9ff8f
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-07-31 17:28:45 -0700

    Update eipresolver.go

--- pkg/networking/eipresolver.go
+++ pkg/networking/eipresolver.go
@@ -8,13 +8,13 @@ import (
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/services"
 	//"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
-type defaultEIPInfoProvider struct {
+type EIPInfoProvider struct {
 	ec2Client        services.EC2
 }
 
 var availableEIPs []string
 var unavailableEIPs []string
-func (p *defaultEIPInfoProvider) eipresolver(EIPnameOrIDs []string) ([]string) {
+func (p *EIPInfoProvider) EIPResolver(EIPnameOrIDs []string) ([]string) {
 	// Creates session object
 	//sess, _ := session.NewSession()
 	// opens a new session
