bc6375dba5517f9788c4305b5177ba91a59ad031
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-09-19 13:02:18 -0700

    Update model_build_load_balancer.go

--- pkg/service/model_build_load_balancer.go
+++ pkg/service/model_build_load_balancer.go
@@ -244,6 +244,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSubnetMappings(_ context.Contex
 	subnetMappings := make([]elbv2model.SubnetMapping, 0, len(ec2Subnets))
 	allocationIDs := networking.EIPResolver(*&eipAllocation)
 	fmt.Printf("allocationIDs",allocationIDs)
+	fmt.Printf(*&eipAllocation)
 	for idx, subnet := range ec2Subnets {
 		mapping := elbv2model.SubnetMapping{
 			SubnetID: aws.StringValue(subnet.SubnetId),
