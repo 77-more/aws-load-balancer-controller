0d5a9ddfb76246e3d9a41002e609e688a89e87ea
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-09-19 13:57:16 -0700

    Update eipresolver.go

--- pkg/networking/eipresolver.go
+++ pkg/networking/eipresolver.go
@@ -14,13 +14,8 @@ import (
 //}
 
 func EIPResolver(EIPnameOrIDs []string) []string {
-	// Creates session object
 	sess, _ := session.NewSession()
-	// opens a new session
 	ec2svc := ec2.New(sess)
-	// makes DescribeAddresses api call and stores the output of type DescribeAddressesOutput in results variable.
-	//As part of the API call we are looking for Name: test1 and cluster-name:test tags
-	// &ec2.DescribeAddressesInput represents the memory address of the
 	var returningEIPs []string
 	if len(EIPnameOrIDs) == 0 {
 		return returningEIPs
@@ -41,11 +36,9 @@ func EIPResolver(EIPnameOrIDs []string) []string {
 			if results.Addresses[0].AssociationId == nil {
 				returningEIPs = append(returningEIPs, nameOrIDs)
 			} else {
-				fmt.Println(nameOrIDs,"has an association ID existing")
 				returningEIPs = append(returningEIPs, nameOrIDs)
 			}
 		} else {
-		fmt.Println("this line is getting executed")
 		results, _ := ec2svc.DescribeAddresses(&ec2.DescribeAddressesInput{
 			Filters: []*ec2.Filter{
 				{
@@ -54,19 +47,10 @@ func EIPResolver(EIPnameOrIDs []string) []string {
 				},
 			},
 		})
-		//if the region is set wrong then the results.Addresses len will be 0 so we need to account for that condition as well.
-		//if len(results.Addresses) == 0 {
-		//	continue
-		//}
-		// *results.Addresses[0].AllocationId is the pointer to the value in AllocationId
-		// results.Addresses[0].AllocationId is the address of the AllocationId field
 		allocationIDs := *results.Addresses[0].AllocationId
-		// the below if condition checks if the AssociationId field exists or no NOTE we are not looking for the address of the field or the value in the address, we are simply looking for the existence of the field itself.
-
 		if results.Addresses[0].AssociationId == nil {
 			returningEIPs = append(returningEIPs, allocationIDs)
 		} else {
-			fmt.Println(nameOrIDs,"has an association ID existing")
 			returningEIPs = append(returningEIPs, allocationIDs)
 		  }
 		}
