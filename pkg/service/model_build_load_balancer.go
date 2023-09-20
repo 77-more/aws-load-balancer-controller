8f46dd2b3f0a14b0c21d2afdaf8f36133d8b42e8
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-09-20 10:39:42 -0700

    Update model_build_load_balancer.go

--- pkg/service/model_build_load_balancer.go
+++ pkg/service/model_build_load_balancer.go
@@ -9,6 +9,7 @@ import (
 	"regexp"
 	"sort"
 	"strconv"
+	"strings"
 
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/algorithm"
 	elbv2deploy "sigs.k8s.io/aws-load-balancer-controller/pkg/deploy/elbv2"
@@ -21,6 +22,7 @@ import (
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/deploy/tracking"
 	elbv2model "sigs.k8s.io/aws-load-balancer-controller/pkg/model/elbv2"
 	"sigs.k8s.io/aws-load-balancer-controller/pkg/networking"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 const (
@@ -72,6 +74,50 @@ func (t *defaultModelBuildTask) buildLoadBalancerSpec(ctx context.Context, schem
 		Tags:                   tags,
 	}
 	return spec, nil
+}
+package networking
+
+import (
+	//"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	//"sigs.k8s.io/aws-load-balancer-controller/pkg/aws/services"
+	//"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"strings"
+)
+//type eipinfoprovider struct {
+//	ec2Client        services.EC2
+//}
+
+func EIPResolver(EIPnameOrIDs []string) []string {
+	sess, _ := session.NewSession()
+	ec2svc := ec2.New(sess)
+	var returningEIPs []string
+	if len(EIPnameOrIDs) == 0 {
+		return returningEIPs
+	}
+	for _, nameOrIDs := range EIPnameOrIDs {
+		if strings.HasPrefix(nameOrIDs, "eipalloc-") {
+				returningEIPs = append(returningEIPs, nameOrIDs)
+		} else {
+		results, _ := ec2svc.DescribeAddresses(&ec2.DescribeAddressesInput{
+			Filters: []*ec2.Filter{
+				{
+					Name:   aws.String("tag:Name"),
+					Values: aws.StringSlice([]string{nameOrIDs}),
+				},
+			},
+		})
+		allocationIDs := *results.Addresses[0].AllocationId
+		if results.Addresses[0].AssociationId == nil {
+			returningEIPs = append(returningEIPs, allocationIDs)
+		} else {
+			returningEIPs = append(returningEIPs, allocationIDs)
+		  }
+		}
+	}
+	return returningEIPs
 }
 
 func (t *defaultModelBuildTask) buildLoadBalancerIPAddressType(_ context.Context) (elbv2model.IPAddressType, error) {
@@ -247,7 +293,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSubnetMappings(_ context.Contex
 			SubnetID: aws.StringValue(subnet.SubnetId),
 		}
 		if eipConfigured {
-			allocationIDs := networking.EIPResolver(*&eipAllocation)
+			allocationIDs := EIPResolver(*&eipAllocation)
 			mapping.AllocationID = aws.String(allocationIDs[idx])
 		}
 		if ipv4AddrConfigured {
