5d067748bd2278e5bed6eaaa47f7dea9d3d6d3a2
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-10-02 13:32:25 -0700

    Update model_build_load_balancer.go

--- pkg/service/model_build_load_balancer.go
+++ pkg/service/model_build_load_balancer.go
@@ -222,7 +222,7 @@ func (t *defaultModelBuildTask) buildLoadBalancerSubnetMappings(_ context.Contex
 				return nil, err
 			}
 			if results.Addresses == nil {
-				return nil, errors.Errorf("EIP",nameOrIDs ,"is not found")
+				return nil, errors.Errorf("No EIP by the name (%d) is found",nameOrIDs)
 			} else {
 				singleallocationID := *results.Addresses[0].AllocationId
 				allocationIDs = append(allocationIDs, singleallocationID)
