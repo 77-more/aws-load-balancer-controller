03f6f22b78759ce902554fcaf8e857e55114e5ae
mourya_zzz <47902676+77-more@users.noreply.github.com>
2023-10-04 17:30:10 -0700

    Update eip_resolver.go

--- pkg/networking/eip_resolver.go
+++ pkg/networking/eip_resolver.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"*/
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/pkg/errors"
 	
 )
